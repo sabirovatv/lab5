4dffdcdf1527d1135d0db020c311e2b9f31d458e
sabirovatv <sabirovatamara77@gmail.com>
2026-03-22 16:11:30 +0000

    upd

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/lab5/third_party/googletest
-# Build directory: /workspaces/lab5/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/lab5/third_party/googletest/googlemock
-# Build directory: /workspaces/lab5/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
